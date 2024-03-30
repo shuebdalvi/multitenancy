9dff24f84a74998cebff4e43883a0e493910d83e
Shueb Dalvi <shueb123@gmail.com>
2024-03-30 11:13:08 +0300

    remove redundant folder

--- multitenancy/src/main/java/com/anakiou/mt/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anakiou.mt.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.anakiou.mt.domain.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
-
-}
